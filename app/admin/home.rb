57b153ba07d7f1527f66a3b27a6ccf3e93559557
Brian Cipriano <cipriano@google.com>
2017-10-29 13:01:03 +0100

    Add new field to the model and update the admin form.

--- app/admin/home.rb
+++ app/admin/home.rb
@@ -4,9 +4,11 @@ ActiveAdmin.register Home do
       f.input :intro, as: :trix_editor
       f.input :featured_page_1
       f.input :featured_page_2
+      f.input :featured_image, as: :file, :input_html => { :class => "s3_url" },
+          :hint => image_tag(f.object.featured_image, style: 'width:200px;height:auto;')
     end
     f.actions
   end
 
-  permit_params :intro, :featured_page_1_id, :featured_page_2_id
+  permit_params :intro, :featured_page_1_id, :featured_page_2_id, :featured_image
 end
