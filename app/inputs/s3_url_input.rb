0085b222fc4abab8befc552787ba1d06d3a2a56a
Brian Cipriano <cipriano@google.com>
2017-11-01 13:29:27 +0100

    Replace table html generation with better version.

--- app/inputs/s3_url_input.rb
+++ app/inputs/s3_url_input.rb
@@ -8,37 +8,30 @@ class S3UrlInput < Formtastic::Inputs::UrlInput
   end
 
   def to_html
-    file_input_cell = template.content_tag :td do
-      builder.hidden_field(method, input_html_options) <<
-      # file_field_tag requires an ID to be given for some reason. Just make sure
-      # it doesn't collide with anything else.
-      template.file_field_tag('attachment_' + SecureRandom.hex(4), class: 's3_url_file')
-    end
-
-    file_input_row = template.content_tag :tr do
-      file_input_cell
-    end
-
-    image_preview_cell = template.content_tag :td do
-      template.image_tag('', class: 's3_url_img', style: 'width:400px;height:auto;') <<
-      template.content_tag(:div, 'Uploading...', class: 's3_url_loading')
-    end
-
-    image_preview_row = template.content_tag :tr do
-      image_preview_cell
-    end
-
-    input_tbody = template.content_tag :tbody do
-      file_input_row << image_preview_row
-    end
-
-    input_table = template.content_tag :table, style: 'display:inline;' do
-      input_tbody
-    end
-
     input_wrapping do
-      label_html <<
-      input_table
+      label_html +
+
+      template.content_tag(:table, style: 'display:inline;') do
+        template.content_tag :tbody do
+
+          template.content_tag(:tr) {
+            template.content_tag :td do
+              builder.hidden_field(method, input_html_options) <<
+              # file_field_tag requires an ID to be given for some reason. Just make sure
+              # it doesn't collide with anything else.
+              template.file_field_tag('attachment_' + SecureRandom.hex(4), class: 's3_url_file')
+            end
+          } +
+
+          template.content_tag(:tr) {
+            template.content_tag :td do
+              template.image_tag('', class: 's3_url_img', style: 'width:400px;height:auto;') <<
+              template.content_tag(:div, 'Uploading...', class: 's3_url_loading')
+            end
+          }
+
+        end
+      end
     end
   end
 end
