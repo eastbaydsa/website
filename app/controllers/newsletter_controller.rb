4a81bad4c4d53ee1a613ecd3aa7918f9272bb580
izaleu <steel.princess@gmail.com>
2017-11-24 13:02:21 -0800

    Adds newsletter signup form.

---
+++ app/controllers/newsletter_controller.rb
@@ -0,0 +1,30 @@
+class NewsletterController < ApplicationController
+    before_action do
+      raise 'You must set NATION_SITE_SLUG in your .env to access this feature.' unless ENV['NATION_SITE_SLUG']
+    end
+  
+    def index
+        #nothing to show
+    end
+
+    def create
+        #POST the newsletter
+
+        userInput = {
+            person: {
+              email: params['email'],
+              #last_name: "Smith",
+              #first_name: "Bob"
+            }
+          }
+          
+        @newsletter_signup = nation_builder_client.call(:people, :push, userInput)
+
+        person_id = @newsletter_signup['person']['id']
+        
+        nation_builder_client.call(:people, :tag_person, {id: person_id, tagging: {tag: 'newsletter'}})
+
+        render :index
+        end
+  end
+  
