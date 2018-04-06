b9d8fe2491de4aa052f86cf1c948f6618e6c7db1
Kyle Schmolze <kyletns@gmail.com>
2018-04-06 11:52:53 -0700

    event page visual update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,18 +12,21 @@ class EventsController < ApplicationController
 
     end_date = @start_date.end_of_month
 
-    @events = nation_builder_client.call(:events, :index, {
-      site_slug: ENV['NATION_SITE_SLUG'],
-      starting: @start_date.to_s,
-      until: end_date.to_s,
-      limit: 1000,
-    })["results"]
-      .select{ |e| e["published_at"] != nil }
-      .map{ |e| Event.new(e) }
+    @events = Event.query(@start_date.to_s, end_date.to_s)
   end
 
   def show
     @event = Event.find(params[:id])
+    @event.description = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    @event.name = "May Day Meetup"
+    @event.address = {
+      'address1' => '1901 Myrtle St.',
+      'address2' => 'Apt 2',
+      'address3' => nil,
+      'city' => 'Oakland',
+      'state' => 'CA',
+      'zip' => '94607'
+    }
   end
 
 end
