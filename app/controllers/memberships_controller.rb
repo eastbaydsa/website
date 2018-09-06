62d8c9467c5c27367852ff8430e348eabfff8670
Kyle Schmolze <kyletns@gmail.com>
2018-09-05 20:48:07 -0700

    check for national membership using nation builder tag

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,8 +3,14 @@ class MembershipsController < ApplicationController
 
   def check
     email = params[:email].to_s.strip.downcase
-    whitelist = ENV['AUTH0_EMAIL_WHITELIST'].split(',')
-    if whitelist.include? email
+
+    # need to rescue from the API call, cause it throws an error if not found
+    person = $nation_builder_client.call(:people, :match, email: email) rescue nil
+    national_member = person['person']['tags'].include?('national_member') rescue false
+    
+    whitelist = ENV['AUTH0_EMAIL_WHITELIST'].to_s.split(',')
+
+    if national_member or whitelist.include?(email)
       render json: { status: :found }, status: :ok
     else
       render json: { status: :not_found }, status: :not_found
@@ -14,7 +20,7 @@ class MembershipsController < ApplicationController
   private
 
   def require_whitelisted_ip
-    return if ENV['AUTH0_IP_WHITELIST'] == 'disabled'
+    return if ENV['AUTH0_IP_WHITELIST'].nil? or ENV['AUTH0_IP_WHITELIST'] == 'disabled'
     whitelist = ENV['AUTH0_IP_WHITELIST'].split(',')
     head :forbidden unless whitelist.include? request.remote_ip
   end
