9c35e0840fe06528d25b7680569f26f99221975a
Kyle Schmolze <kyletns@gmail.com>
2018-09-06 09:56:44 -0700

    install rails settings cached, set up activeadmin GUI page, added first setting: ability to disable the hardcoded homepage or use a Page

---
+++ app/admin/setting.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register_page 'Setting' do
+  title = 'Settings'
+  menu label: title
+  display = {
+    homepage_hardcoded: :boolean
+  }
+  active_admin_settings_page(title: title, display: display)
+end
