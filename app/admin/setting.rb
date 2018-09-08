3e3b17c6a3cdadc89339fb0a0556c29dc996634f
Kyle Schmolze <kyletns@gmail.com>
2018-09-07 19:29:03 -0700

    make search bar a setting

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register_page 'Setting' do
   title = 'Settings'
   menu label: title
   display = {
-    homepage_hardcoded: :boolean
+    homepage_hardcoded: :boolean,
+    search_in_footer: :boolean
   }
   active_admin_settings_page(title: title, display: display)
 end
