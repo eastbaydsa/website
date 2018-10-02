4459a77d9e03b75eff200c155085d79d707be5e3
Dominic Dagradi <dominic@chain.com>
2018-10-01 22:40:29 -0700

    update settings

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register_page 'Setting' do
   title = 'Settings'
   menu label: title
   display = {
-    homepage_hardcoded: :boolean
+    homepage_hardcoded: :boolean,
+    homepage_image_url: :string
   }
   active_admin_settings_page(title: title, display: display)
 end
